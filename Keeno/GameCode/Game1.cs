28f1ecd8e8ab465a6e399cd94d04923298e88253
gdliba <147038453+gdliba@users.noreply.github.com>
2025-04-16 12:23:05 +0100

    Added GameStates and GameCode Folder

---
+++ Keeno/GameCode/Game1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Keeno
+{
+    public enum GameState
+    {
+        Start,
+        Playing,
+        GameOver
+    }
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        #region VARIABLES
+
+        // Game State
+        private GameState currentGameState;
+
+
+        #endregion
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Window.Title = "Keeno";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // Match the resolution to the current display
+            _graphics.PreferredBackBufferWidth = 
+                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = 
+                GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            // Set screen to Fullscreen
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
+
+
+            currentGameState = GameState.Start;
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            // GameState Switch
+            switch (currentGameState)
+            {
+                case GameState.Start:
+                    StartUpdate();
+                    break;
+                case GameState.Playing:
+                    PlayingUpdate();
+                    break;
+                case GameState.GameOver:
+                    GameOverUpdate();
+                    break;
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            switch (currentGameState)
+            {
+                case GameState.Start:
+                    StartDraw();
+                    break;
+                case GameState.Playing:
+                    PlayingDraw();
+                    break;
+                case GameState.GameOver:
+                    GameOverDraw();
+                    break;
+            }
+
+            base.Draw(gameTime);
+        }
+        #region STATE UPDATES
+        private void StartUpdate()
+        {
+
+        }
+
+        private void PlayingUpdate()
+        {
+
+        }
+
+        private void GameOverUpdate()
+        {
+
+        }
+        #endregion
+        #region STATE DRAWS
+        private void StartDraw()
+        {
+
+        }
+
+        private void PlayingDraw()
+        {
+
+        }
+
+        private void GameOverDraw()
+        {
+
+        }
+        #endregion
+    }
+}
