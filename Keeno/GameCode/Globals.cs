01aa7892fd0cc5d6fda4357eac58d468b7afb142
gdliba <147038453+gdliba@users.noreply.github.com>
2026-09-02 18:50:56 +0100

    Update Globals.cs

--- Keeno/GameCode/Globals.cs
+++ Keeno/GameCode/Globals.cs
@@ -98,7 +98,7 @@ namespace Keeno
         {
             Graphics.PreferredBackBufferWidth = width;
             Graphics.PreferredBackBufferHeight = height;
-            Graphics.IsFullScreen = true;
+            Graphics.IsFullScreen = false;
             Graphics.ApplyChanges();
         }
         public static void Update(GameTime gt)
